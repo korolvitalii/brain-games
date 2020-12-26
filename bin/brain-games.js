c25c26cf2f8d5b08e59461424a42803ef8983b57
Vitalii Korol <korolvitalik@gmail.com>
2020-12-26 14:13:04 +0100

    added eslint+configuration

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+// import { ESLint } from 'eslint';
 import readlineSync from 'readline-sync';
-import welcomeText from '../src/cli.js'
+import welcomeText from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('What is your name: ');
 welcomeText(name);
