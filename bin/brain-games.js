6a436305c7d665e4e7b13c4ff0f8720487b4c5c5
Vitalii Korol <korolvitalik@gmail.com>
2020-12-26 10:53:10 +0100

    added library

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import welcomeText from '../src/cli.js'
 console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('What is your name: ');
+welcomeText(name);
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+const welcomeText = (name) => console.log(`Hello ${name}, your Welcome!`);
+
+export default welcomeText;
