8051102ad8676c2570bed6024a5d646a31c6061b
Vitalii Korol <korolvitalik@gmail.com>
2021-01-01 10:05:57 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default (game) => {
   const [description, getQuestionAndAnswer] = game();
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('What is your name: ');
-  console.log(`Hello ${playerName}, you Welcome!`);
+  console.log(`Hello, ${playerName}!`);
   console.log(description);
   const count = 3;
   for (let i = 0; i < count; i += 1) {
