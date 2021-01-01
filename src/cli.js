f2fa7037a9a9021682597f88521ef96ce01f12e5
Vitalii Korol <korolvitalik@gmail.com>
2021-01-01 10:28:29 +0100

    fixed problems for cheking

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('What is your name: ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello ${name}, your Welcome!`);
 };
 
